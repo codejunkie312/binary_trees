51a6aa5032bcaeee66266164d5d9b56783ba8f14
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-12-02 00:19:46 +0100

    added task 103

--- binary_trees.h
+++ binary_trees.h
@@ -83,5 +83,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 /* 102-binary_tree_is_complete.c */
 int binary_tree_is_complete(const binary_tree_t *tree);
+/* 103-binary_tree_rotate_left.c */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
--- main.c
+++ main.c
@@ -36,36 +36,21 @@ void launch_test(binary_tree_t *n1, binary_tree_t *n2)
 int main(void)
 {
     binary_tree_t *root;
-    int complete;
 
     root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
     root->right = binary_tree_node(root, 128);
-    root->left->right = binary_tree_node(root->left, 54);
-    root->right->right = binary_tree_node(root, 402);
-    root->left->left = binary_tree_node(root->left, 10);
-
+    root->right->right = binary_tree_node(root->right, 402);
     binary_tree_print(root);
-    complete = binary_tree_is_complete(root);
-    printf("Is %d complete: %d\n", root->n, complete);
-    complete = binary_tree_is_complete(root->left);
-    printf("Is %d complete: %d\n", root->left->n, complete);
-
-    root->right->left = binary_tree_node(root->right, 112);
+    printf("Rotate-left %d\n", root->n);
+    root = binary_tree_rotate_left(root);
     binary_tree_print(root);
-    complete = binary_tree_is_complete(root);
-    printf("Is %d complete: %d\n", root->n, complete);
+    printf("\n");
 
-    root->left->left->left = binary_tree_node(root->left->left, 8);
+    root->right->right = binary_tree_node(root->right, 450);
+    root->right->left = binary_tree_node(root->right, 420);
     binary_tree_print(root);
-    complete = binary_tree_is_complete(root);
-    printf("Is %d complete: %d\n", root->n, complete);
-
-    root->left->right->left = binary_tree_node(root->left->right, 23);
+    printf("Rotate-left %d\n", root->n);
+    root = binary_tree_rotate_left(root);
     binary_tree_print(root);
-    complete = binary_tree_is_complete(root);
-    printf("Is %d complete: %d\n", root->n, complete);
-
-    binary_tree_delete(root);
     return (0);
 }
