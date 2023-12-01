2e266662db3f08f7cd5b2c4ac54a85004d6984a4
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-12-01 03:14:53 +0100

    fixed issues

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_root - checks if a given node is a root
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a root, otherwise 0
+ * If node is NULL, return 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
+}
+
+/**
  * binary_tree_depth - measures the depth of a node in a binary tree
  * @tree: pointer to the node to measure the depth
  * Return: depth of the node, or 0 if tree is NULL
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
+ * If node is NULL, return 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if ((node->left == NULL) && (node->right == NULL))
+		return (1);
+	else
+		return (0);
+}
+
+/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree
  * Return: height of the tree, or 0 if tree is NULL
