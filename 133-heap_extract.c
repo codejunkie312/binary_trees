e2fa773b3f7f686f392643045e773027abb59107
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-12-02 03:47:57 +0100

    added task 133

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,97 +1,140 @@
 #include "binary_trees.h"
 
 /**
- * swap_values - swaps the values of two integers
- * @a: pointer to the first integer
- * @b: pointer to the second integer
- * Return: nothing
- */
-void swap_values(int *a, int *b)
+ * enqueue_133 - enqueues a binary tree node to a queue
+ * @queue: double pointer to the queue to enqueue the node in
+ * @node: pointer to the binary tree node to enqueue
+ * Return: pointer to the created queue node, or NULL on failure
+*/
+queue_t *enqueue_133(queue_t **queue, binary_tree_t *node)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	queue_t *new_node, *tmp;
+
+	new_node = malloc(sizeof(queue_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->node = node;
+	new_node->next = NULL;
+
+	if (*queue == NULL)
+		*queue = new_node;
+	else
+	{
+		tmp = *queue;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new_node;
+	}
+
+	return (new_node);
+}
+
+/**
+ * dequeue_133 - dequeues a binary tree node from a queue
+ * @queue: double pointer to the queue to dequeue the node from
+ * Return: pointer to the dequeued node, or NULL on failure
+*/
+binary_tree_t *dequeue_133(queue_t **queue)
+{
+	queue_t *tmp;
+	binary_tree_t *node;
+
+	if (*queue == NULL)
+		return (NULL);
+
+	tmp = *queue;
+	*queue = (*queue)->next;
+	node = tmp->node;
+	free(tmp);
+	return (node);
 }
 
 /**
  * get_last_node - gets the last node of a binary tree
- * @root: pointer to the root node of the tree
- * Return: pointer to the last node, or NULL on failure
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: pointer to the last node, or NULL if tree is NULL
  */
-heap_t *get_last_node(heap_t *root)
+heap_t *get_last_node(heap_t *tree)
 {
-    if (root == NULL)
-        return NULL;
+	queue_t *queue = NULL;
+	heap_t *node;
 
-    while (root->left != NULL || root->right != NULL)
-    {
-        if (root->right != NULL)
-            root = root->right;
-        else
-            root = root->left;
-    }
+	if (tree == NULL)
+		return (NULL);
 
-    return root;
+	enqueue_133(&queue, tree);
+
+	while (queue != NULL)
+	{
+		node = dequeue_133(&queue);
+		if (node->left != NULL)
+			enqueue_133(&queue, node->left);
+		if (node->right != NULL)
+			enqueue_133(&queue, node->right);
+	}
+
+	return (node);
 }
-
 
 /**
  * heapify_down - heapifies a binary tree
- * @root: pointer to the root node of the tree
- * Return: pointer to the last node, or NULL on failure
+ * @root: pointer to the root node of the tree to heapify
+ * Return: nothing
  */
 void heapify_down(heap_t *root)
 {
-    heap_t *largest = root;
-    heap_t *left = root->left;
-    heap_t *right = root->right;
+	heap_t *largest = root, *left = root->left, *right = root->right;
+	int temp;
 
-    if (left != NULL && left->n > largest->n)
-        largest = left;
+	if (left != NULL && left->n > largest->n)
+		largest = left;
+	if (right != NULL && right->n > largest->n)
+		largest = right;
 
-    if (right != NULL && right->n > largest->n)
-        largest = right;
-
-    if (largest != root)
-    {
-        swap_values(&(root->n), &(largest->n));
-        heapify_down(largest);
-    }
+	if (largest != root)
+	{
+		temp = root->n;
+		root->n = largest->n;
+		largest->n = temp;
+		heapify_down(largest);
+	}
 }
-
 
 /**
  * heap_extract - extracts the root node of a Max Binary Heap
  * @root: double pointer to the root node of the heap
- * Return: value stored in the root node, or 0 on failure
+ * Return: value stored in the root node, or 0 if root is NULL
  */
 int heap_extract(heap_t **root)
 {
-    int extracted_value;
-    heap_t *last_node;
-    
-    if (root == NULL || *root == NULL)
-        return 0;
+	int root_value;
+	heap_t *last_node;
 
-    extracted_value = (*root)->n;
-    last_node = get_last_node(*root);
+	if (*root == NULL || root == NULL)
+		return (0);
 
-    if (*root == last_node)
-    {
-        free(*root);
-        *root = NULL;
-        return extracted_value;
-    }
+	root_value = (*root)->n;
+	last_node = get_last_node(*root);
 
-    (*root)->n = last_node->n;
+	if (last_node != *root)
+	{
+		(*root)->n = last_node->n;
 
-    if (last_node->parent->left == last_node)
-        last_node->parent->left = NULL;
-    else
-        last_node->parent->right = NULL;
+		if (last_node->parent->left == last_node)
+			last_node->parent->left = NULL;
+		else
+			last_node->parent->right = NULL;
 
-    free(last_node);
-    heapify_down(*root);
+		free(last_node);
+		heapify_down(*root);
+	}
+	else
+	{
+		free(*root);
+		*root = NULL;
+	}
 
-    return extracted_value;
+	return (root_value);
 }
